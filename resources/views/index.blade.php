bdb1c09a06d698748b4ac2e2c5e953cc661dc347
Syahrul ANDRI <syahrul_173040161@mail.unpas.ac.id>
2019-12-04 02:34:00 +0700

    membuat folder LAYOUT pada folder Views dan membamhkan file MAIN.BLADE.PHP di folder layout

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+<div class="container">
+    <div class="row">
+        <div class="col-10">
+            
+            <h1 class="mt-3">Hello, world!</h1>
+
+        </div>
+    </div>
+</div>
+    
+
+   
